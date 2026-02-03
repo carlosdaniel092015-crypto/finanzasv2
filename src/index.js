4296afe60aeb44a2cc8e80404122e9f3d3c66848
Usuario <usuario@ejemplo.com>
2026-02-03 14:32:18 -0400

    Fix merge conflicts in src/index.js and src/firebase.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -8,15 +7,4 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> 9184a8321a30628f749dc70b1fc5793b7870f75a
 );
