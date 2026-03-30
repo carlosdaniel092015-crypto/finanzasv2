5f82aa73d0a673c9049aef53000b743b0410d74e
carlosdaniel092015-crypto <carlosdaniel092015@gmail.com>
2026-03-30 13:44:27 -0400

    Update instrument.js

--- src/instrument.js
+++ src/instrument.js
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: "https://2627892d70b946fc945748fb8516159b@n8n-bugsink.jyrqup.easypanel.host/1",
+  dsn: "https://2627892d70b946fc945748fb8516159b@bugsink.acentosdeco.lat/1",
   integrations: [],
   tracesSampleRate: 1.0,
 });
